b8a72320e9aa0c0424ba43d7f7fa529fa4c34dcd
UnNoble <UnNoble235@users.noreply.github.com>
2020-01-19 18:54:58 +0100

    Canvis de noms
    vddf

---
+++ Project/Assets/Scripts_C#/Adder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    public void add()
+    {
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
