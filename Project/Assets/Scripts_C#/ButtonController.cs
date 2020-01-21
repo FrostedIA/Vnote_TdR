f980ca2a879b75dd44bbe32b28c1efb9ad572d25
UnNoble <UnNoble235@users.noreply.github.com>
2020-01-21 19:09:29 +0100

    Mecaniques_Acabades

--- Project/Assets/Scripts_C#/ButtonController.cs
+++ Project/Assets/Scripts_C#/ButtonController.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class ButtonController : MonoBehaviour
 {
     public GameObject ByePanel;
+    public GameObject theText1;
     public GameObject theText;
     public void ClearText()
     {
+        theText1.GetComponent<InputField>().text = "";
         theText.GetComponent<InputField>().text = "";
-       
-}
+
+    }
 
 
 
