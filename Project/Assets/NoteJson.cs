d215b81b9bd1e9a0b06b3c3731a466923f3723b7
UnNoble <UnNoble235@users.noreply.github.com>
2020-03-19 10:40:29 +0100

    def

--- Project/Assets/NoteJson.cs
+++ Project/Assets/NoteJson.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class NoteJson : MonoBehaviour
 {
@@ -10,14 +11,46 @@ public class NoteJson : MonoBehaviour
     public Transform content;
     public GameObject addPanel;
     public Button createButton;
+    
     public GameObject NotePrefab;
     public GameObject Thing;
     string filePath;
-   
+    public Animator Content;
+
 
     private List<sample1> checkNotes = new List<sample1>();
 
     private InputField[] addInputFields;
+
+    public class ChecklistItem
+    {
+        public string NoteName;
+        public string type; 
+        public int index1;
+
+        public ChecklistItem(string name,string type,int index)
+        {
+            this.NoteName = name;
+            this.type = type;
+            this.index1 = index;
+
+
+        }
+
+
+
+
+    }
+
+    public void DELETE()
+    {
+        Content.SetTrigger("DELET");
+    }
+    public void NORMAL()
+    {
+        Content.SetTrigger("sfg");
+    }
+
 
 
 
@@ -25,9 +58,16 @@ public class NoteJson : MonoBehaviour
     private void Start()
     {
         filePath = Application.persistentDataPath + "/note.txt";
+        loadJSONdata();
+        
         addInputFields = addPanel.GetComponentsInChildren<InputField>();
         createButton.onClick.AddListener(delegate { CreateCheckNoteItem(addInputFields[0].text, addInputFields[1].text); }  );
+      
+
+
+
     }
+  
 
    public  void SwitchMode(int mode)
     {
@@ -59,29 +99,85 @@ public class NoteJson : MonoBehaviour
     }
 
 
-    void CreateCheckNoteItem(string name, string type)
+    void CreateCheckNoteItem(string name, string type,int loadIndex = 0,bool loading = false)
         {
             GameObject item = Instantiate(NotePrefab); 
 
             item.transform.SetParent(content);
         item.transform.localScale = new Vector3(1, 1, 1);
        sample1 itemObject = item.GetComponent<sample1>();
-        int index = 0;
-        if (checkNotes.Count > 0)
-       index = checkNotes.Count - 1;
-
-
+     
+      int index = loadIndex;
+        if (!loading)
+            index = checkNotes.Count;
             itemObject.SetObjecInfo(name, type, index);
             checkNotes.Add(itemObject);
         sample1 temp = itemObject;
-   
+       
+        
+        itemObject.GetComponent<Button>().onClick.AddListener(delegate { CheckItem(temp); }) ;
+      if (!loading)
+        {
+          SaveJSONdata();
         SwitchMode(0);    
     
+        }
+      
     
+    }
+     
+
+    void CheckItem(sample1 item)
+    {
+        Debug.Log("clar");
+        checkNotes.Remove(item);
+        SaveJSONdata();
+        Destroy(item.gameObject);
     }
 
 
 
+
+
+
+
+    void SaveJSONdata()
+    {
+        string content = "";
+        for(int i = 0;  i < checkNotes.Count; i++)
+        {
+            ChecklistItem temp = new ChecklistItem(checkNotes[i].NoteName,checkNotes[i].type,checkNotes[i].index1);
+            content += JsonUtility.ToJson(temp) + "\n";
+            Debug.Log("chi");
+        }
+        File.WriteAllText(filePath, content);
+
+
+    }
+    void loadJSONdata()
+    {
+        if (File.Exists(filePath))
+        {
+            string contents = File.ReadAllText(filePath);
+            string[] splitContents = contents.Split('\n');
+            foreach (string content in splitContents) 
+            {
+                if (content.Trim() != "")
+                {
+   ChecklistItem temp = JsonUtility.FromJson<ChecklistItem>(content.Trim());
+                CreateCheckNoteItem(temp.NoteName, temp.type,temp.index1,true);
+                }
+            
+            } 
+
+        }
+        else 
+        { 
+            Debug.Log("no fileeeeeeee");
+        }
+       
+
+    }
 
 
 
---
+++ Project/Assets/Scripts_C#/Sample2.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Sample2 : MonoBehaviour
+{
+    public string NoteName;
+    public string type;
+    public int index1;
+    public Text title;
+    public Text body;
+    public GameObject ru;
+    public GameObject ru2;
+    public string Dtext;
+    public string Mtext;
+    public Button createButton;
+    private List<sample1> checkNotes = new List<sample1>();
+    public GameObject NotePrefab;
+    private InputField[] addInputFields;
+    public GameObject addPanel;
+    public GameObject Itself;
+    string filePath;
+    void Start()
+    {
+        filePath = Application.persistentDataPath + "/note.txt";
+        ru.GetComponent<InputField>().text =NoteName;
+        ru2.GetComponent<InputField>().text = type;
+        addInputFields = addPanel.GetComponentsInChildren<InputField>();
+        createButton.onClick.AddListener(delegate { CreateCheckNoteItem(addInputFields[0].text, addInputFields[1].text); });
+
+    }
+    public void Savenote()
+    {
+        Dtext = title.GetComponent<Text>().text;
+        PlayerPrefs.SetString("Title", Dtext);
+        Mtext = body.GetComponent<Text>().text;
+        PlayerPrefs.SetString("Note", Mtext);
+        NoteName = Dtext;
+        type = Mtext;
+
+    }
+
+    void CreateCheckNoteItem(string name, string type)
+    {
+        GameObject item = Instantiate(NotePrefab);
+
+        item.transform.SetParent(GameObject.FindGameObjectWithTag("Content").transform, false);
+        item.transform.localScale = new Vector3(1, 1, 1);
+        sample1 itemObject = item.GetComponent<sample1>();
+        int index = index1;
+        if (checkNotes.Count > 0)
+            index = checkNotes.Count - 1;
+
+        
+        itemObject.SetObjecInfo(name, type, index);
+        checkNotes.Add(itemObject);
+        sample1 temp = itemObject;
+        SaveJSONdata();
+        Destroy(Itself);
+    }
+
+    public class ChecklistItem
+    {
+        public string NoteName;
+        public string type;
+        public int index1;
+
+        public ChecklistItem(string name, string type, int index)
+        {
+            this.NoteName = name;
+            this.type = type;
+            this.index1 = index;
+
+
+        }
+
+
+
+
+    }
+
+    void SaveJSONdata()
+    {
+        string content = "";
+        for (int i = 0; i < checkNotes.Count; i++)
+        {
+            ChecklistItem temp = new ChecklistItem(checkNotes[i].NoteName, checkNotes[i].type, checkNotes[i].index1);
+            content += JsonUtility.ToJson(temp) + "\n";
+            Debug.Log("chi");
+        }
+        File.WriteAllText(filePath, content);
+
+
+    }
+
+
+
+
+
+    public void SetObjecInfo(string name, string type, int index)
+    {
+        this.NoteName = name;
+        this.type = type;
+        this.index1 = index;
+
+
+
+
+    }
+}
